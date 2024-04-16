c48f61c666e9351c0bb9c6fa93798f1b5870fbe0
flower636 <kizilovaas001@gmail.com>
2024-04-16 23:48:58 +0300

    Updated

--- main.py
+++ main.py
@@ -6,13 +6,6 @@ from io import BytesIO
 
 app = Flask(__name__)
 
-@app.route("/")
-def root():
-   return '''<form action="/qr" method="GET">
-      <input type="text" name="msg" placeholder="Enter text"/>
-      <input type="submit" value="QR-code"/>
-   </form>'''
-
 @app.route("/qr")
 def qr():
    msg = request.args.get('msg')
@@ -23,6 +16,7 @@ def qr():
  
    img64 = base64.b64encode(buffer.getvalue())
    return f'<img src="data:image/png;base64, {img64.decode()}" alt="qrcode" />'
+
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0')
